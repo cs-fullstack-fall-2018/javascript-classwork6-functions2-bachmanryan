82ab2cc313ee68856914fdac83385496ef95e5e9
Ryan Bachman <jacksprat455@gmail.com>
2018-07-31 13:15:32 -0500

    Methods_pt3

---
+++ pt1.js
@@ -0,0 +1,10 @@
+function main() {
+    var favNumber = parseInt(prompt("Enter your favorite number: "));
+    alert("Your favorite number is: " + favNumber);
+}
+// function number() {
+//     alert("Your favorite number is: " + favNumber);
+//
+// }
+
+main();
